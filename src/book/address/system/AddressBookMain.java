3a2d11cfab10be9a52e1125f0aea7b6f12880f25
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-03 14:00:59 +0530

    Added: Welcome message

---
+++ src/book/address/system/AddressBookMain.java
@@ -0,0 +1,10 @@
+package book.address.system;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program!");		
+	}
+}
