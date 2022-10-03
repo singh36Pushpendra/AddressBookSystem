fa3a99626882213236454520be2750be3cf80dc1
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-03 14:15:33 +0530

    Added: UC1 Conatacts of person

---
+++ src/book/address/system/ContactPerson.java
@@ -0,0 +1,25 @@
+package book.address.system;
+
+public class ContactPerson {
+	private String firstName, lastName, email, address, city, state;
+	private int zip;
+	private long phonNum;
+
+	ContactPerson(String firstName, String lastName, String email, String address, String city, String state, int zip,
+			long phonNum) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phonNum = phonNum;
+	}
+
+	public String toString() {
+		return "Person Name: " + firstName + " " + lastName + "\nPerson Address: " + address + ", " + city + ", "
+				+ state + "\nPerson Email: " + email + "\nPerson Postal Code: " + zip + "\nPerson Phone number: "
+				+ phonNum;
+	}
+}
